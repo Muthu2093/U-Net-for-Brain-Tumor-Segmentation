5d5f38403df9aef42e8b7d324ed0dbd7991712f1
Muthu2093 <muthuvel@buffalo.edu>
2018-04-23 22:58:08 -0400

    Modifed data loading for unet
    Modifed data loading for unet

--- code/get_input_data.py
+++ code/get_input_data.py
@@ -9,6 +9,12 @@ from medpy.io import save
 
 PATH = "/Users/muthuvel/Desktop/Independent Study - M Gao/Dataset/BRATS-train/Image_Data"
 
+PATH_Synthetic = "/Users/muthuvel/Desktop/Independent Study - M Gao/Dataset/BRATS-train/Synthetic_Data"
+
+PATH_test = "/Users/muthuvel/Desktop/Independent Study - M Gao/Dataset/BRATS-test/Image_Data"
+
+PATH_test_Synthetic = "/Users/muthuvel/Desktop/Independent Study - M Gao/Dataset/BRATS-test/Synthetic_Data"
+
 def pad_image(img, desired_shape=(256, 256)):
     pad_top = 0
     pad_bot = 0
@@ -58,15 +64,16 @@ def create_4_chan_data(t2, t1c, t1, flair, ot):
 
 
 def load_dataset(path):
-    train_t2 = []
-    train_t1c = []
-    train_t1 = []
-    train_flair = []
-    train_ot = []
+    train_t2 = np.zeros([176,256,256,1])
+    train_t1c = np.zeros([176,256,256,1])
+    train_t1 = np.zeros([176,256,256,1])
+    train_flair = np.zeros([176,256,256,1])
+    train_ot = np.zeros([176,256,256,1])
 
     for dir in os.listdir(path):
         if dir == 'HG':
             HG_path = os.path.join(path, 'HG')
+            i=1
             for dir2 in os.listdir(HG_path):
                 if dir2 != '.DS_Store':
                     HG_t2 = load_single_image(os.path.join(HG_path, dir2, 'VSD.Brain.XX.O.MR_T2'))
@@ -76,11 +83,33 @@ def load_dataset(path):
                     HG_ot = load_single_image(os.path.join(HG_path, dir2, 'VSD.Brain_3more.XX.XX.OT'))
                     assert (HG_ot.shape == HG_flair.shape == HG_t1.shape == HG_t1c.shape == HG_t2.shape)
                     HG_samples = create_4_chan_data(HG_t2, HG_t1c, HG_t1, HG_flair, HG_ot)
-                    train_ot.append(HG_samples[0])
-                    train_flair.append(HG_samples[1])
-                    train_t1.append(HG_samples[2])
-                    train_t1c.append(HG_samples[3])
-                    train_t2.append(HG_samples[4])
+                    print(HG_samples[0].shape)
+                    if (HG_samples[0].shape != (176, 256, 256)):
+                        continue
+                    
+                    a = np.reshape(HG_samples[0],[176,256,256,1])
+                    train_ot = np.concatenate((train_ot,a),axis = 3)
+                    
+                    a = np.reshape(HG_samples[1],[176,256,256,1])
+                    train_flair = np.concatenate((train_flair,a),axis = 3)
+                    
+                    a = np.reshape(HG_samples[2],[176,256,256,1])
+                    train_t1 = np.concatenate((train_t1,a),axis = 3)
+                    
+                    a = np.reshape(HG_samples[3],[176,256,256,1])
+                    train_t1c = np.concatenate((train_t1c,a),axis = 3)
+                    
+                    a = np.reshape(HG_samples[4],[176,256,256,1])
+                    train_t2 = np.concatenate((train_t2,a),axis = 3)
+                    
+                    #rain_ot.append(HG_samples[0])
+                    #train_flair.append(HG_samples[1])
+                    #train_t1.append(HG_samples[2])
+                    #train_t1c.append(HG_samples[3])
+                    #train_t2.append(HG_samples[4])
+                    i=i+1
+                    print(i)
+                    
         if dir == 'LG':
             brain_1 = brain_2 = brain_3 = False
             LG_path = os.path.join(path, 'LG')
@@ -103,17 +132,37 @@ def load_dataset(path):
 
                     assert (LG_ot.shape == LG_flair.shape == LG_t1.shape == LG_t1c.shape == LG_t2.shape)
                     LG_samples = create_4_chan_data(LG_t2, LG_t1c, LG_t1, LG_flair, LG_ot)
-                    train_ot.append(LG_samples[0])
-                    train_flair.append(LG_samples[1])
-                    train_t1.append(LG_samples[2])
-                    train_t1c.append(LG_samples[3])
-                    train_t2.append(LG_samples[4])
+                    #a = np.reshape(HG_samples[0],[176,256,256,1])
+                    print(LG_samples[0].shape)
+                    if (LG_samples[0].shape != (176, 256, 256)):
+                        continue
+                    a = np.reshape(HG_samples[0],[176,256,256,1])
+                    train_ot = np.concatenate((train_ot,a),axis = 3)
+                    
+                    a = np.reshape(HG_samples[1],[176,256,256,1])
+                    train_flair = np.concatenate((train_flair,a),axis = 3)
+                    
+                    a = np.reshape(HG_samples[2],[176,256,256,1])
+                    train_t1 = np.concatenate((train_t1,a),axis = 3)
+                    
+                    a = np.reshape(HG_samples[3],[176,256,256,1])
+                    train_t1c = np.concatenate((train_t1c,a),axis = 3)
+                    
+                    a = np.reshape(HG_samples[4],[176,256,256,1])
+                    train_t2 = np.concatenate((train_t2,a),axis = 3)
+                    
+                    #train_ot = np.concatenate((train_ot,a),axis = 3)
+                    #train_flair.append(LG_samples[1])
+                    #train_t1.append(LG_samples[2])
+                    #train_t1c.append(LG_samples[3])
+                    #train_t2.append(LG_samples[4])
+    
     # Stacking all individual layers
-    train_ot = np.vstack(train_ot)
-    train_flair = np.vstack(train_flair)
-    train_t1 = np.vstack(train_t1)
-    train_t1c = np.vstack(train_t1c)
-    train_t2 = np.vstack(train_t2)
+    #train_ot = np.vstack(train_ot)
+    #train_flair = np.vstack(train_flair)
+    #train_t1 = np.vstack(train_t1)
+    #train_t1c = np.vstack(train_t1c)
+    #train_t2 = np.vstack(train_t2)
     assert (train_ot.shape == train_flair.shape == train_t1.shape == train_t1c.shape == train_t2.shape)
     return [train_t2, train_t1c,train_t1,train_flair,train_ot]
 
@@ -121,3 +170,22 @@ def load_dataset(path):
 ##img=mpimg.imread('your_image.png')
 [train_t2, train_t1c,train_t1,train_flair,train_ot] = load_dataset(PATH)
 
+[train_t2_s, train_t1c_s,train_t1_s,train_flair_s,train_ot_s] = load_dataset(PATH_Synthetic)
+
+[test_t2, test_t1c,test_t1,test_flair,test_ot] = load_dataset(PATH_test)
+
+[test_t2_s, test_t1c_s,test_t1_s,test_flair_s,test_ot_s] = load_dataset(PATH_test_Synthetic)
+
+np.save('../data/train_flair',train_flair)
+np.save('../data/train_ot',train_ot)
+np.save('../data/train_t1',train_t1)
+np.save('../data/train_t1c',train_t1c)
+np.save('../data/train_t2',train_t2)
+
+np.save('../data/test_flair',test_flair)
+np.save('../data/test_ot',test_ot)
+np.save('../data/test_t1',test_t1)
+np.save('../data/test_t1c',test_t1c)
+np.save('../data/test_t2',test_t2)
+
+[train_t2, train_t1c,train_t1,train_flair,train_ot] = load_dataset(PATH)
